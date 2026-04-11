03644eceda4215c26b0627288af0d8801bb379fe
code-760 <himanshu760kumawat@gmail.com>
2026-04-11 18:23:35 +0530

    fix orderRoutes

--- server/App/routes/admin/adminRoutes.js
+++ server/App/routes/admin/adminRoutes.js
@@ -15,10 +15,11 @@ const { SubsubcategoryRoutes } = require("./SubsubcategoryRoutes");
 const { ProductRoutes } = require("./productRoutes");
 const userRoutes = require("./userRoutes");
 
-const oderRoutes = require("./orderRoutes");
+// const oderRoutes = require("./orderRoutes");
 const AdminRoutes = require("./AdminuserRoutes");
-
 const oderroutes = require("./oderroutes");
+
+
 
 let adminRoutes = express.Router();
 
